19c0142045f4c48cd21f4ae9baf981edac492bf2
Destiny-wei <weihaoming0418@gmail.com>
2023-10-24 21:03:12 +0800

    Connect to TON

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import { Address, TonClient } from "ton"
+import { Address, TonClient } from "ton";
 import { getHttpEndpoint } from "@orbs-network/ton-access";
+import { BN } from 'bn.js';
 
 async function main() {
 
@@ -17,6 +18,22 @@ async function main() {
     const miningData = await client.callGetMethod(collection, 'get_mining_data');
 
     console.log(miningData);
+
+    const parseStackNum = (sn: any) => new BN(sn[1].substring(2), 'hex');
+
+    const complexity = parseStackNum(miningData.stack[0]);
+    const last_success = parseStackNum(miningData.stack[1]);
+    const seed = parseStackNum(miningData.stack[2]);
+    const target_delta = parseStackNum(miningData.stack[3]);
+    const min_cpl = parseStackNum(miningData.stack[4]);
+    const max_cpl = parseStackNum(miningData.stack[5]);
+
+    console.log('complexity', complexity);
+    console.log('last_success', last_success.toString());
+    console.log('seed', seed);
+    console.log('target_delta', target_delta.toString());
+    console.log('min_cpl', min_cpl.toString());
+    console.log('max_cpl', max_cpl.toString());
 }
 
 main()
